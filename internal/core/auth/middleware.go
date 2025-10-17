b46ce00bfa46792a322d3ad8f1cfc8814a3984b6
tolstovrob <tolstovrob@gmail.com>
2025-10-17 11:08:08 +0400

    feat(core/auth): add http middleware

---
+++ internal/core/auth/middleware.go
@@ -0,0 +1,55 @@
+package auth
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/tolstovrob/knocknock/internal/core/sessions"
+)
+
+/*
+ * Тут содержится код дял http middleware системы авторизации, а также соответствующая работа с контекстом
+ */
+
+type contextKey string
+
+const sessionContextKey contextKey = "session"
+
+func (a *Auth) Middleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			token := extractToken(r)
+			if session, err := a.GetSession(r.Context(), token); err == nil && token != "" {
+				ctx := context.WithValue(r.Context(), sessionContextKey, session)
+				r = r.WithContext(ctx)
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func GetSession(ctx context.Context) *sessions.Session {
+	if session, ok := ctx.Value(sessionContextKey).(*sessions.Session); ok {
+		return session
+	}
+	return nil
+}
+
+func extractToken(r *http.Request) string {
+	// Из заголовка HTTP
+	if authHeader := r.Header.Get("Authorization"); authHeader != "" && len(authHeader) > 7 && authHeader[:7] == "Bearer " {
+		return authHeader[7:]
+	}
+
+	// Из query параметра
+	if token := r.URL.Query().Get("token"); token != "" {
+		return token
+	}
+
+	// Из cookie
+	if cookie, err := r.Cookie("session_token"); err == nil {
+		return cookie.Value
+	}
+
+	return ""
+}
