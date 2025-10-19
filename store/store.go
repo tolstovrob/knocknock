637d1780725909ddf67e50531647f87fd19f91ea
tolstovrob <tolstovrob@gmail.com>
2025-10-19 18:13:33 +0400

    docs(store): remove old comments

--- store/store.go
+++ store/store.go
@@ -6,11 +6,6 @@ import (
 	"github.com/tolstovrob/knocknock/sessions"
 )
 
-/*
- * Интерфейс для взаимодействия с базой данных. Программист может выбрать любую базу и реализовать свои методы для
- * работы с ней
- */
-
 type Store interface {
 	Save(ctx context.Context, session *sessions.Session) error
 	Get(ctx context.Context, token string) (*sessions.Session, error)
