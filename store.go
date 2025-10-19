5d51454a2886f0d1edcb52e2d2d04746e6d8612a
tolstovrob <tolstovrob@gmail.com>
2025-10-19 22:33:54 +0400

    docs(store): add go doc comments

--- store.go
+++ store.go
@@ -1,9 +1,15 @@
 package knocknock
 
+/*
+ * В store.go описан интерфейс для реализации методов работы с любым хранилищем. Библиотека поставляет лишь in-memory
+ * хранилище (store_memory.go), оставляя остальное на усмотрение программиста
+ */
+
 import (
 	"context"
 )
 
+// Интерфейс для хранилища сессий. Реализации должны обеспечивать сохранение, получение и удаление сессий.
 type Store interface {
 	Save(ctx context.Context, session *Session) error
 	Get(ctx context.Context, token string) (*Session, error)
