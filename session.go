d74420f7569399455ac8ff50589ed736d56c3898
tolstovrob <tolstovrob@gmail.com>
2025-10-19 22:25:48 +0400

    docs(session): add go doc comments

--- session.go
+++ session.go
@@ -1,22 +1,36 @@
 package knocknock
 
+/*
+ * session.go содержит структуру сессии и сопутствующие методы
+ */
+
 import "time"
 
-type UserData = any
+// UserData представляет пользовательские данные, хранимые в сессии. Может быть любого типа.
+type UserData = any // TODO(tolstovrob): think about generics
 
+// Структура сессии
 type Session struct {
-	Token     string    `json:"token"`
-	UserData  UserData  `json:"userData"`
-	CreatedAt time.Time `json:"createdAt"`
-	ExpiresAt time.Time `json:"expiresAt"`
+	Token     string    `json:"token"`     // Токен сессии
+	UserData  UserData  `json:"userData"`  // Информация о пользователе
+	CreatedAt time.Time `json:"createdAt"` // Время создания
+	ExpiresAt time.Time `json:"expiresAt"` // Время истечения
 }
 
+// Создает новую сессию с указанным токеном, пользовательскими данными и сроком жизни. Важно: третий аргумент expiresIn
+// отвечает именно за время жизни, а значит в структуре Session в поле createdAt будет текущее время, а в expiresAt --
+// текущее время + expiresIn.
+//
+// Пример:
+//
+//	session := sessions.New("token123", userData, 6*time.Hour)
 func MakeSession(token string, userData UserData, expiresIn time.Duration) *Session {
 	now := time.Now()
 	ss := &Session{Token: token, UserData: userData, CreatedAt: now, ExpiresAt: now.Add(expiresIn)}
 	return ss
 }
 
+// Проверяет, истекла ли сессия
 func (s *Session) IsExpired() bool {
 	return time.Now().After(s.ExpiresAt)
 }
